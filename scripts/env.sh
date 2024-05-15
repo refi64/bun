d3f4f7f96dc61a6309aca3d0d4db3f0e0a45c270
Ashcon Partovi <ashcon@partovi.net>
2024-05-15 10:31:58 -0700

    Add debug 3

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-echo "whoami: $(whoami)"
-echo "shell: $SHELL"
-echo "bash: $BASH"
+# hack for buildkite
+if [[ "${CI:-}" == "1" || "${CI:-}" == "true" ]]; then
+  if [[ $(uname -s) == 'Darwin' ]]; then
+    export BUN_INSTALL="$HOME/.bun"
+    export PATH=$BUN_INSTALL/bin:$PATH
+    export PATH="$(brew --prefix llvm@16)/bin:$PATH"
+  fi
+fi
 
 # this is the environment script for building bun's dependencies
 # it sets c compiler and flags
