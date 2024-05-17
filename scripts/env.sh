8e6d96ee101a1338ded0633a959d7af1b5916ba9
Ashcon Partovi <ashcon@partovi.net>
2024-05-17 11:08:20 -0700

    Bring back hack

--- scripts/env.sh
+++ scripts/env.sh
@@ -7,6 +7,9 @@ if [[ "${CI:-}" == "1" || "${CI:-}" == "true" ]]; then
     export PATH="$BUN_INSTALL/bin:$PATH"
     export PATH="$(brew --prefix llvm@16)/bin:$PATH"
   fi
+  if [[ $(uname -s) == 'Linux' ]]; then
+    source ~/.bashrc || true
+  fi
 fi
 
 # this is the environment script for building bun's dependencies
